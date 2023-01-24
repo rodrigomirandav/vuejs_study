f5861f915ec0213fbe460cfd9d28bd2b01ce12c5
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-01-24 18:37:44 -0300

    Single File Components

---
+++ Real World Vue 3/real-world-vue/src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import EventCard from "@/components/EventCard.vue";
+
+import { ref } from "Vue";
+
+const events = ref([
+  {
+    id: 5928101,
+    category: "animal welfare",
+    title: "Cat Adoption Day",
+    description: "Find your new feline friend at this event.",
+    location: "Meow Town",
+    date: "January 28, 2022",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Kat Laydee",
+  },
+  {
+    id: 598253,
+    category: "animal welfare",
+    title: "Cat Adoption Day",
+    description: "Find your new feline friend at this event.",
+    location: "Meow Town",
+    date: "January 28, 2022",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Kat Laydee",
+  },
+  {
+    id: 1066952,
+    category: "animal welfare",
+    title: "Cat Adoption Day",
+    description: "Find your new feline friend at this event.",
+    location: "Meow Town",
+    date: "January 28, 2022",
+    time: "12:00",
+    petsAllowed: true,
+    organizer: "Kat Laydee",
+  },
+]);
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
