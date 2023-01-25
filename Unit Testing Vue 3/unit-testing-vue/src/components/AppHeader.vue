b97d0419aa06e3c447feb5b6b92a65d3b5c4ca4f
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-01-25 15:35:27 -0300

    Writting a Unit Test Jest

---
+++ Unit Testing Vue 3/unit-testing-vue/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: true,
+    };
+  },
+};
+</script>
+
+<style></style>
