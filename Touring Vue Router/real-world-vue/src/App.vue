21a4e702d81d9022a18bb60501b53c85013ddc4c
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-01-25 02:12:53 -0300

    Flash Message

--- Touring Vue Router/real-world-vue/src/App.vue
+++ Touring Vue Router/real-world-vue/src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <div id="flashMessage" v-if="GStore.flashMessage">
+    {{ GStore.flashMessage }}
+  </div>
+
   <nav>
     <router-link :to="{ name: 'EventList' }">Events</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
@@ -6,7 +10,27 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  inject: ["GStore"],
+}
+</script>
+
 <style>
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
