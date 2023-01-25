de9edd4218eacd5fe144e3071a97dfcd601a78d7
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2023-01-25 11:37:09 -0300

    Program with Pinia Init

---
+++ Pinia Fundamentals/todo-app/src/components/TodoApp.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TodoForm from "../components/TodoForm.vue"
+import TodoList from "../components/TodoList.vue"
+</script>
+
+<template>
+    <div class="todo-app">
+        <h1>To Do List</h1>
+        <todo-form />
+        <todo-list />
+    </div>
+</template>
+
+<style scoped>
+</style>
